093cdc48348e23f166c25e2753809199e457dcc6
VarunSolanki4 <109731729+VarunSolanki4@users.noreply.github.com>
2023-08-17 19:08:47 +0530

    Changes in Study Permit Page

--- src/Pages/study_permit.js
+++ src/Pages/study_permit.js
@@ -49,7 +49,7 @@ function Home(){
       <Header />
         <div className="banner_section">
           <img src={Img.student_visa_banner} className="banner_img" alt=""></img>
-          <button className="embark_book_appoint">Book An Appointment</button>         
+          <button className="embark_book_appoint">Book An Appointment</button>
         </div>
         <div className="immigrate_canada_section" id="top">
           <div className="container-fluid">
@@ -60,10 +60,10 @@ function Home(){
               <div className="col-md-6">
                 <p className="study_permit_para">A study permit is a document issued by the Canadian government that allows foreign nationals to study at designated learning institutions (DLIs) in the country. It serves as an essential requirement for most international students planning to pursue their academic aspirations in Canada.</p>
                 <p className="study_permit_para">To ensure a smooth and successful study permit application process, it is imperative to have all the required documents ready beforehand.</p>
-                <p className="study_permit_para">The specific documents necessary may vary depending on your unique circumstances, including whether you are applying through the Student Direct Stream (SDS) or the regular non-SDS stream.</p>              
+                <p className="study_permit_para">The specific documents necessary may vary depending on your unique circumstances, including whether you are applying through the Student Direct Stream (SDS) or the regular non-SDS stream.</p>
               </div>
               <div className="col-md-6">
-                <img src={Img.study_permit} className="study_permit_img" alt=""></img>               
+                <img src={Img.study_permit} className="study_permit_img" alt=""></img>
               </div>
             </div>
           </div>
@@ -76,18 +76,38 @@ function Home(){
               </div>
               <div className="col-md-6">
                 <div className="sdm_box">
-                  <h3 className="sdm_stream_title">SDS Stream</h3>
+                  <h3 className="sdm_stream_title">Student Direct Stream</h3>
+                  <ul>
+                    <li className="sdm_stream_ul_li">Only for Study Permit</li>
+                    <li className="sdm_stream_ul_li">Visa Processing within 20 days</li>
+                    <li className="sdm_stream_ul_li">Only Online Application Submíssion</li>
+                    <li className="sdm_stream_ul_li">Mandatory GIC of at least 10,000 or more</li>
+                    <li className="sdm_stream_ul_li">Applicable to certain Asian and African countries</li>
+                    <li className="sdm_stream_ul_li">up to 85% of Sucess Rate</li>
+                    <li className="sdm_stream_ul_li">IELTS mandatory</li>
+                    <li className="sdm_stream_ul_li">1-year Tuition Fee to be paid in advancez</li>
+                  </ul>
                 </div>
               </div>
               <div className="col-md-6">
                 <div className="sdm_box">
                   <h3 className="sdm_stream_title">Non- SDS Stream</h3>
+                  <ul>
+                    <li className="sdm_stream_ul_li">For Canada Study/Work Permit and Immigration Purposes</li>
+                    <li className="sdm_stream_ul_li">Visa Processing wit in 45-90 days</li>
+                    <li className="sdm_stream_ul_li">Online and Offline Application Submission</li>
+                    <li className="sdm_stream_ul_li">GIC not a mandatory requirement</li>
+                    <li className="sdm_stream_ul_li">Applicable to candidates from any country</li>
+                    <li className="sdm_stream_ul_li">up to 65% of Success Rate</li>
+                    <li className="sdm_stream_ul_li">IELTS, PTE or TIEFL accepted</li>
+                    <li className="sdm_stream_ul_li">6-Months Tuition Fee to be paid in advance</li>
+                  </ul>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        
+
         <div className="why_glowing_section">
           <div className="container-fluid">
             <div className="row align_item_center">
@@ -107,9 +127,9 @@ function Home(){
         <div className="contact_us_today_section">
            <div className="container-fluid">
               <div className="row align_item_center">
-                <div className="col-md-12">                
+                <div className="col-md-12">
                   <h3 className="contact_us_today_text">Contact us today to start your journey <br /> towards a world-class education in Canada.</h3>
-                  <button className="video_book_appoint mt_10">Book An Appointment</button>                  
+                  <button className="video_book_appoint mt_10">Book An Appointment</button>
                 </div>
               </div>
            </div>
